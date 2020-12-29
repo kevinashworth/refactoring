9e9b4863bda0c4771701e79e2ee21646bc399245
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-28 19:34:22 -0800

    Add a second invoice with twice the number of audience
    - Add some semistandard'ing
    - Continue to ignore original code in `statement.js`

--- test/statement.js
+++ test/statement.js
@@ -1,3 +1,4 @@
+/* eslint-env mocha */
 const assert = require('assert');
 
 const invoices = require('../invoices');
@@ -16,6 +17,20 @@ Amount owed is $1,730.00
 You earned 47 credits
 `;
       const invoice = statement(invoices[0], plays);
+      assert.equal(invoice, expected);
+    });
+  });
+
+  describe('Invoice 1', function () {
+    it('should return the expected statement', function () {
+      const expected = `Statement for BigCo2
+  Hamlet: $1,200.00 (110 seats)
+  As You Like It: $860.00 (70 seats)
+  Othello: $900.00 (80 seats)
+Amount owed is $2,960.00
+You earned 184 credits
+`;
+      const invoice = statement(invoices[1], plays);
       assert.equal(invoice, expected);
     });
   });
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var assert = require('assert');
+/* eslint-env mocha */
+const assert = require('assert');
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
