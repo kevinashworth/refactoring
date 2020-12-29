493aa6dc3d9dd625206e85c724366d4b5d3d2c63
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-29 08:18:03 -0800

    Create HTML version of statement. Add tests.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const invoices = require('./invoices');
 const plays = require('./plays');
 
-const statement = require('./statement');
+const statement = require('./statement').statement;
+const htmlStatement = require('./statement').htmlStatement;
 
 console.log(statement(invoices[0], plays));
+console.log(htmlStatement(invoices[0], plays));
--- test/statement.js
+++ test/statement.js
@@ -4,10 +4,11 @@ const assert = require('assert');
 const invoices = require('../invoices');
 const plays = require('../plays');
 
-const statement = require('../statement');
+const statement = require('../statement').statement;
+const htmlStatement = require('../statement').htmlStatement;
 
-describe('Invoice Statement', function () {
-  describe('Invoice 0', function () {
+describe('Invoice Statements', function () {
+  describe('Invoice 0 (plain)', function () {
     it('should return the expected statement', function () {
       const expected = `Statement for BigCo
   Hamlet: $650.00 (55 seats)
@@ -21,7 +22,7 @@ You earned 47 credits
     });
   });
 
-  describe('Invoice 1', function () {
+  describe('Invoice 1 (plain)', function () {
     it('should return the expected statement', function () {
       const expected = `Statement for BigCo2
   Hamlet: $1,200.00 (110 seats)
@@ -31,6 +32,39 @@ Amount owed is $2,960.00
 You earned 184 credits
 `;
       const invoice = statement(invoices[1], plays);
+      assert.equal(invoice, expected);
+    });
+  });
+  describe('Invoice 0 (html)', function () {
+    it('should return the expected statement', function () {
+      const expected = `<h1>Statement for BigCo</h1>
+<table>
+  <tr><th>play</th><th>seats</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is $1,730.00</p>
+<p>You earned 47 credits</p>
+`;
+      const invoice = htmlStatement(invoices[0], plays);
+      assert.equal(invoice, expected);
+    });
+  });
+
+  describe('Invoice 1 (html)', function () {
+    it('should return the expected statement', function () {
+      const expected = `<h1>Statement for BigCo2</h1>
+<table>
+  <tr><th>play</th><th>seats</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>110</td><td>$1,200.00</td></tr>
+  <tr><td>As You Like It</td><td>70</td><td>$860.00</td></tr>
+  <tr><td>Othello</td><td>80</td><td>$900.00</td></tr>
+</table>
+<p>Amount owed is $2,960.00</p>
+<p>You earned 184 credits</p>
+`;
+      const invoice = htmlStatement(invoices[1], plays);
       assert.equal(invoice, expected);
     });
   });
