21d564a015db1ab3cce5f08d5eb03022edb1dbb8
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-29 07:19:55 -0800

    Extract Function (with random name appleSauce)

--- statement.js
+++ statement.js
@@ -5,10 +5,7 @@ function statement (invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
-  let totalAmount = 0;
-  for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf);
-  }
+  let totalAmount = appleSauce();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -61,6 +58,14 @@ function statement (invoice, plays) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
+  }
+
+  function appleSauce () {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
   }
 }
 
