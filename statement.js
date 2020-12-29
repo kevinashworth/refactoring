a1334fc08ed0fb4bdb84caac196bd2130c520562
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-29 07:48:16 -0800

    Move Function - playFor part 2

--- statement.js
+++ statement.js
@@ -19,7 +19,7 @@ function renderPlainText (data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
   result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -28,7 +28,7 @@ function renderPlainText (data, plays) {
 
   function amountFor (aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -48,14 +48,10 @@ function renderPlainText (data, plays) {
     return result;
   }
 
-  function playFor (aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function volumeCreditsFor (aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
