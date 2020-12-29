ebd3a12b826e0a24ff94952ad6a699c2376598a8
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-28 19:45:45 -0800

    Replace Temp with Query

--- statement.js
+++ statement.js
@@ -9,7 +9,7 @@ function statement (invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -45,6 +45,10 @@ function statement (invoice, plays) {
         throw new Error(`unknown type: ${play.type}`);
     }
     return result;
+  }
+
+  function playFor (aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
